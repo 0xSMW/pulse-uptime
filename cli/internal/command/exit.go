a67e64bec3a02212576165a21944a604b24c2b06
Stephen <stephen@klu.ai>
2026-07-18 12:13:37 +0700

    cli: define stable process exit codes

---
+++ cli/internal/command/exit.go
@@ -0,0 +1,16 @@
+package command
+
+const (
+	ExitSuccess         = 0
+	ExitUnexpected      = 1
+	ExitInvalidInput    = 2
+	ExitAuthentication  = 3
+	ExitConditionFailed = 4
+	ExitNotFound        = 5
+	ExitConflict        = 6
+	ExitPermission      = 7
+	ExitRateLimited     = 8
+	ExitUnavailable     = 9
+	ExitPartialSuccess  = 10
+	ExitInterrupted     = 130
+)
