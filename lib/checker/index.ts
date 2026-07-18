93fe4aeff02be328b85452ce879bfd920b0da224
Stephen <stephen@klu.ai>
2026-07-18 12:10:53 +0700

    checker: expose secure checker API

---
+++ lib/checker/index.ts
@@ -0,0 +1,6 @@
+export * from "./checker";
+export * from "./classify-error";
+export * from "./ip-policy";
+export * from "./secure-lookup";
+export * from "./types";
+export * from "./validation";
