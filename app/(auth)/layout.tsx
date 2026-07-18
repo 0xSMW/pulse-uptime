6a40388c5ee4ebbda37e28c005d867e5d2b5f643
Stephen <stephen@klu.ai>
2026-07-18 12:20:55 +0700

    auth: add focused route layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from "react";
+
+import { AuthShell } from "./shell";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <AuthShell>{children}</AuthShell>;
+}
+
