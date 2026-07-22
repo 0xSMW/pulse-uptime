46122b6aa0ee1505f28bbf3a737fc82cf3610d6a
Stephen <stephen@klu.ai>
2026-07-22 20:11:46 +0700

    Release 0.3.1 (#20)

--- cli/internal/buildinfo/version.go
+++ cli/internal/buildinfo/version.go
@@ -2,7 +2,7 @@ package buildinfo
 
 import "runtime"
 
-var Version = "0.3.0"
+var Version = "0.3.1"
 
 func UserAgent() string {
 	return "pulsectl/" + Version + " (" + runtime.GOOS + "; " + runtime.GOARCH + ")"
